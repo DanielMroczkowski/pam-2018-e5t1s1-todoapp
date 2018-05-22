026ec6dbc9ddfa6c413061cac48b04daee8aebfd
sikorapatryk <sikorapatryk@mail.com>
2018-05-22 13:00:36 +0200

    fixes

--- app/src/main/java/pl/wat/e5t1s1/todoapp/MainActivity.java
+++ app/src/main/java/pl/wat/e5t1s1/todoapp/MainActivity.java
@@ -1,27 +1,33 @@
 package pl.wat.e5t1s1.todoapp;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.wat.e5t1s1.todoapp.db.TaskContract;
+import pl.wat.e5t1s1.todoapp.db.TaskDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    private ArrayAdapter<String> mAdapter;
+    RecyclerView mTaskListView;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+    private TaskDbHelper mHelper;
 
     /**
      * Utworzenie TAGu dla aktywności MainActivity,
@@ -29,37 +35,107 @@ public class MainActivity extends AppCompatActivity {
      */
     private static final String TAG = "MainActivity";
 
-    /**
-     * Obsługa przycisku dodaj zadanie
-     *
-     * @param item
-     * @return Logcat entry
-     */
-    @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.action_add_task:
-                    final EditText taskEditText = new EditText(this);
-                    AlertDialog dialog = new AlertDialog.Builder(this)
-                            .setTitle("Nowe zadanie")
-                            .setMessage("Co planujesz?")
-                            .setView(taskEditText)
-                            .setPositiveButton("Dodaj", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    String task = String.valueOf(taskEditText.getText());
-                                    Log.d(TAG, "Zadanie do dodania: " + task);
-                                }
-                            })
-                            .setNegativeButton("Anuluj", null)
-                            .create();
-                    dialog.show();
-                    return true;
 
-                default:
-                    return super.onOptionsItemSelected(item);
-            }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mHelper = new TaskDbHelper(MainActivity.this);
+        mTaskListView = findViewById(R.id.lista);
+
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+
+        while(cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            Log.d(TAG, "Task: " + cursor.getString(idx));
         }
+        cursor.close();
+        db.close();
+        updateUI();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                /**
+                 * Obsługa przycisku dodaj zadanie
+                 *
+                 * @param item
+                 * @return boolean
+                 */
+                final EditText taskEditText = new EditText(MainActivity.this);
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Nowe zadanie")
+                        .setMessage("Co planujesz?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Dodaj", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
+                            }
+                        })
+                        .setNegativeButton("Anuluj", null)
+                        .create();
+                updateUI();
+                dialog.show();
+            }
+        });
+    }
+
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(idx));
+        }
+
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(MainActivity.this,
+                    R.layout.position,
+                    R.id.task_title,
+                    taskList);
+            //mTaskListView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
+    }
+
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextView = parent.findViewById(R.id.task_title);
+        String task = String.valueOf(taskTextView.getText());
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        db.delete(TaskContract.TaskEntry.TABLE,
+                TaskContract.TaskEntry.COL_TASK_TITLE + " = ?",
+                new String[]{task});
+        db.close();
+        updateUI();
+    }
 
 
 }
